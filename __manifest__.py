9f93c7c062c34b20082624995454fe40bb2a3184
Mathieu CLAVEL <math.clavel@gmail.com>
2017-03-22 15:07:57 +0100

    add menu and view

--- __manifest__.py
+++ __manifest__.py
@@ -3,4 +3,10 @@
   'description': 'User friendly features.',
   'author': 'Mathieu CLAVEL',
   'depends': ['todo_user'],
+  
+  'data': [ 
+    # 'security/ir.model.access.csv', 
+    'views/todo_view.xml', 
+    'views/todo_menu.xml', 
+  ]
 }
